793f4a82373c227a9cd763164f4e8043456df331
Gary Clarke <garyclarke1974@gmail.com>
2024-04-13 10:43:46 +0100

    Add problem to lsp

--- liskov-substitution.php
+++ liskov-substitution.php
@@ -4,6 +4,12 @@ require_once 'vendor/autoload.php';
 
 use App\User;
 
+// We should be able to replace a class with a subclass without any unexpected behavior.
+
+// What problem does this solve
+// - Ensures that subclasses can be used interchangeably with their base classes without altering the functionality or correctness of the program
+// - Prevents the use of inheritance for quick n dirty hacks.
+
 $user = new User('info@garyclarke.tech', '@garyclarketech');
 
 function loginUser(User $user) {
