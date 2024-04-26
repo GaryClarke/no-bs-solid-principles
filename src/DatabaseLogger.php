8374722482d97d1d61e4bcd117cf2d11c298779e
Gary Clarke <garyclarke1974@gmail.com>
2024-04-26 17:41:51 +0100

    Remove rogue semi-colon

--- src/DatabaseLogger.php
+++ src/DatabaseLogger.php
@@ -9,6 +9,6 @@ class DatabaseLogger implements LoggerInterface
     public function log(string $message): void
     {
         // Log message to a database..faked here
-        echo "Log entry added to database: $message" . PHP_EOL;;
+        echo "Log entry added to database: $message" . PHP_EOL;
     }
 }
